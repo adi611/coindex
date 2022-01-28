28ca96cb7bd852065a95059194357c0acd11ed65
adi <50960175+adi611@users.noreply.github.com>
2022-01-29 02:34:03 +0530

    add files

---
+++ bin/coindex.js
@@ -0,0 +1,8 @@
+#!/usr/bin/env node
+const program = require("commander");
+const pkg = require("../package.json")
+
+program
+    .version(pkg.version)
+    .command('key', 'Manage API Key -- hhtps://nomics.com')
+    .parse(process.argv) // has access to arguments we pass wsing this
